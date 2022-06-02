c0fa729e82bd14475cb1640c9e7b3d02a0378557
gabriel2342 <gabriel.pendas@gmail.com>
2022-06-01 20:35:14 -0600

    Nested muni model and routes with org

--- app/controllers/municipalities_controller.rb
+++ app/controllers/municipalities_controller.rb
@@ -1,6 +1,7 @@
 class MunicipalitiesController < ApplicationController
   before_action :set_municipality, only: %i[ show edit update destroy ]
   before_action :authenticate_user!
+  before_action :set_organization
 
   # GET /municipalities or /municipalities.json
   def index
@@ -14,6 +15,7 @@ class MunicipalitiesController < ApplicationController
   # GET /municipalities/new
   def new
     @municipality = Municipality.new
+
   end
 
   # GET /municipalities/1/edit
@@ -22,11 +24,14 @@ class MunicipalitiesController < ApplicationController
 
   # POST /municipalities or /municipalities.json
   def create
+
     @municipality = Municipality.new(municipality_params)
+    @municipality.organization = @organization
+
 
     respond_to do |format|
       if @municipality.save
-        format.html { redirect_to municipality_url(@municipality), notice: "Municipality was successfully created." }
+        format.html { redirect_to organization_municipality_url(@organization, @municipality), notice: "Municipality was successfully created." }
         format.json { render :show, status: :created, location: @municipality }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -39,7 +44,7 @@ class MunicipalitiesController < ApplicationController
   def update
     respond_to do |format|
       if @municipality.update(municipality_params)
-        format.html { redirect_to municipality_url(@municipality), notice: "Municipality was successfully updated." }
+        format.html { redirect_to organization_municipality_url(@organization, @municipality), notice: "Municipality was successfully updated." }
         format.json { render :show, status: :ok, location: @municipality }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -53,7 +58,7 @@ class MunicipalitiesController < ApplicationController
     @municipality.destroy
 
     respond_to do |format|
-      format.html { redirect_to municipalities_url, notice: "Municipality was successfully destroyed." }
+      format.html { redirect_to organization_municipalities_url(@organization), notice: "Municipality was successfully destroyed." }
       format.json { head :no_content }
     end
   end
@@ -64,8 +69,12 @@ class MunicipalitiesController < ApplicationController
       @municipality = Municipality.find(params[:id])
     end
 
+    def set_organization
+      @organization = Organization.find(params[:organization_id])
+    end
+
     # Only allow a list of trusted parameters through.
     def municipality_params
-      params.require(:municipality).permit(:organization_id, :name)
-    end
+      params.require(:municipality).permit(:name, organizations_attributes: [:id, :name, :_destroy])
+    end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :organizations do
-    post :addMunicipality, on: :member
-  end
-      
-  resources :service_providers
+    resources :municipalities 
+    end
+    
+    resources :service_providers
+
   
   get 'about', to: 'pages#about'
   root 'pages#home'
