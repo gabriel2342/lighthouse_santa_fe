5df39f503b0ae09609f4dccc1fb3ba530892f1f8
gabriel2342 <gabriel.pendas@gmail.com>
2022-06-06 06:58:26 -0600

    Formatted organizations index and added turbo frames

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   def home
     @service_providers = ServiceProvider.all
     @service_providers = ServiceProvider.search(params[:search])
+
+    @municipalities = Municipality.all
+    @municipalities = Municipality.search(params[:search])
   end
 
   def about
--- app/models/municipality.rb
+++ app/models/municipality.rb
@@ -7,4 +7,12 @@ class Municipality < ApplicationRecord
   has_many :service_providers, dependent: :destroy, inverse_of: :municipality
 
   accepts_nested_attributes_for :service_providers, allow_destroy: true
+
+
+  def self.search(search)
+    if search
+    else
+      all
+    end
+  end
 end
