a022393ce73a2d6eff0d832af367a9b7fa2399b3
gabriel2342 <gabriel.pendas@gmail.com>
2022-06-26 08:26:36 -0600

    Formatted service provders form

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,7 +13,7 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
-
+  config.action_controller.include_all_helpers = true
   # Enable server timing
   config.server_timing = true
 
