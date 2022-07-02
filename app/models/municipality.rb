e37ad2ef07bb8cc7e1a4e0170052963be8051be0
gabriel2342 <gabriel.pendas@gmail.com>
2022-07-02 17:04:06 -0600

    Updated user model realtionship w/ org/muni models

--- app/models/municipality.rb
+++ app/models/municipality.rb
@@ -1,6 +1,5 @@
 class Municipality < ApplicationRecord
   belongs_to :organization, optional: true
-  belongs_to :user, optional: true
   validates_presence_of :name, presence: true 
   validates_presence_of :organization_id, presence: true
 
